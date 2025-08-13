8b7032e2e10677b4d24b86642b4e1270dc289868
Saifullah <saifullah.saifullah@dfki.uni-kl.de>
2025-08-13 19:20:38 +0200

    Refactor evaluations API: add sample explanations and metrics handling

--- src/atria_hub/api/evaluations.py
+++ src/atria_hub/api/evaluations.py
@@ -1,64 +1,156 @@
 from __future__ import annotations
 
+import io
+import json
 import uuid
 from typing import TYPE_CHECKING, Any
 from uuid import UUID
 
 from atria_core.logger.logger import get_logger
+from atriax_client.types import File
 
 from atria_hub.api.base import BaseApi
 from atria_hub.client import AtriaHubClient
 from atria_hub.exceptions import api_error_handler
 
 if TYPE_CHECKING:
-    from atriax_client.models.model_evaluation import ModelEvaluation
+    from atriax_client.models.evaluation_experiment import EvaluationExperiment
 
 logger = get_logger(__name__)
 
 
-class EvaluationSampleResultApi(BaseApi):
+class SampleExplanationsApi(BaseApi):
     @api_error_handler
-    def list_indices(self, evaluation_id: UUID) -> list[int]:
-        """Read a batch of sample results from an evaluation."""
-        from atriax_client.api.sample_results import sample_results_list_indices
+    def write(
+        self,
+        evaluation_experiment_id: UUID,
+        explainer_name: str,
+        explainer_config: dict[str, Any],
+        sample_index: int,
+        data: bytes,
+    ) -> None:
+        """Write a sample result to an evaluation."""
+        from atriax_client.api.sample_explanations import sample_explanations_write
+        from atriax_client.models.body_sample_explanations_write import (
+            BodySampleExplanationsWrite,
+        )
 
         with self._client.protected_api_client as client:
-            return sample_results_list_indices.sync_detailed(
-                client=client, evaluation_id=evaluation_id
+            return sample_explanations_write.sync_detailed(
+                client=client,
+                evaluation_experiment_id=evaluation_experiment_id,
+                body=BodySampleExplanationsWrite(
+                    sample_index=sample_index,
+                    explainer_name=explainer_name,
+                    config=json.dumps(explainer_config),
+                    explanation_file=File(
+                        payload=io.BytesIO(data),
+                        file_name="explanation.bin",
+                        mime_type="application/octet-stream",
+                    ),
+                ),
+            )
+
+    @api_error_handler
+    def read(self, evaluation_experiment_id: UUID, sample_index: int) -> list[dict]:
+        """Read a batch of sample results from an evaluation."""
+        from atriax_client.api.sample_explanations import sample_explanations_read
+
+        with self._client.protected_api_client as client:
+            return sample_explanations_read.sync_detailed(
+                client=client,
+                evaluation_experiment_id=evaluation_experiment_id,
+                sample_index=sample_index,
+            )
+
+
+class SampleExplanationMetricsApi(BaseApi):
+    def __init__(self, client: AtriaHubClient):
+        self._client = client
+
+    @api_error_handler
+    def write(self, evaluation_experiment_id: UUID, metrics: dict[str, Any]) -> None:
+        """Write a sample result to an evaluation."""
+        from atriax_client.api.sample_explanation_metrics import (
+            sample_explanation_metrics_write,
+        )
+        from atriax_client.models.sample_explanation_metric_create import (
+            SampleExplanationMetricCreate,
+        )
+
+        logger.info("metrics %s", metrics)
+        with self._client.protected_api_client as client:
+            return sample_explanation_metrics_write.sync_detailed(
+                client=client,
+                evaluation_experiment_id=evaluation_experiment_id,
+                body=[
+                    SampleExplanationMetricCreate(
+                        key=key,
+                        value=SampleExplanationMetricCreateValue(**value),
+                        config=config,
+                    )
+                    for key, value in metrics.items()
+                ],
+            )
+
+    @api_error_handler
+    def read(self, evaluation_experiment_id: UUID) -> list[dict]:
+        """Read a batch of sample results from an evaluation."""
+        from atriax_client.api.metrics import metrics_read
+
+        with self._client.protected_api_client as client:
+            return metrics_read.sync_detailed(
+                client=client, evaluation_experiment_id=evaluation_experiment_id
+            )
+
+
+class SampleEvaluationApi(BaseApi):
+    @api_error_handler
+    def list_indices(self, evaluation_experiment_id: UUID) -> list[int]:
+        """Read a batch of sample results from an evaluation."""
+        from atriax_client.api.sample_evaluations import sample_evaluations_list_indices
+
+        with self._client.protected_api_client as client:
+            return sample_evaluations_list_indices.sync_detailed(
+                client=client, evaluation_experiment_id=evaluation_experiment_id
             )
 
     @api_error_handler
     def write(
-        self, evaluation_id: UUID, data_per_sample_index: dict[int, dict]
+        self, evaluation_experiment_id: UUID, data_per_sample_index: dict[int, dict]
     ) -> None:
         """Write a sample result to an evaluation."""
-        from atriax_client.api.sample_results import sample_results_write
+        from atriax_client.api.sample_evaluations import sample_evaluations_write
         from atriax_client.models import (
-            EvaluationSampleResultCreate,
-            EvaluationSampleResultCreateData,
+            SampleEvaluationCreate,
+            SampleEvaluationCreateData,
         )
 
         with self._client.protected_api_client as client:
-            return sample_results_write.sync_detailed(
+            return sample_evaluations_write.sync_detailed(
                 client=client,
-                evaluation_id=evaluation_id,
+                evaluation_experiment_id=evaluation_experiment_id,
                 body=[
-                    EvaluationSampleResultCreate(
+                    SampleEvaluationCreate(
                         sample_index=sample_index,
-                        data=EvaluationSampleResultCreateData.from_dict(data),
+                        data=SampleEvaluationCreateData.from_dict(data),
                     )
                     for sample_index, data in data_per_sample_index.items()
                 ],
             )
 
     @api_error_handler
-    def read(self, evaluation_id: UUID, sample_indices: list[int]) -> list[dict]:
+    def read(
+        self, evaluation_experiment_id: UUID, sample_indices: list[int]
+    ) -> list[dict]:
         """Read a batch of sample results from an evaluation."""
-        from atriax_client.api.sample_results import sample_results_read
+        from atriax_client.api.sample_evaluations import sample_evaluations_read
 
         with self._client.protected_api_client as client:
-            return sample_results_read.sync_detailed(
-                client=client, evaluation_id=evaluation_id, body=sample_indices
+            return sample_evaluations_read.sync_detailed(
+                client=client,
+                evaluation_experiment_id=evaluation_experiment_id,
+                body=sample_indices,
             )
 
 
@@ -67,7 +159,7 @@ class EvaluationMetricsApi(BaseApi):
         self._client = client
 
     @api_error_handler
-    def write(self, evaluation_id: UUID, metrics: dict[str, Any]) -> None:
+    def write(self, evaluation_experiment_id: UUID, metrics: dict[str, Any]) -> None:
         """Write a sample result to an evaluation."""
         from atriax_client.api.metrics import metrics_write
         from atriax_client.models.evaluation_metric_create import EvaluationMetricCreate
@@ -76,7 +168,7 @@ class EvaluationMetricsApi(BaseApi):
         with self._client.protected_api_client as client:
             return metrics_write.sync_detailed(
                 client=client,
-                evaluation_id=evaluation_id,
+                evaluation_experiment_id=evaluation_experiment_id,
                 body=[
                     EvaluationMetricCreate(key=key, value=value)
                     for key, value in metrics.items()
@@ -84,31 +176,43 @@ class EvaluationMetricsApi(BaseApi):
             )
 
     @api_error_handler
-    def read(self, evaluation_id: UUID) -> list[dict]:
+    def read(self, evaluation_experiment_id: UUID) -> list[dict]:
         """Read a batch of sample results from an evaluation."""
         from atriax_client.api.metrics import metrics_read
 
         with self._client.protected_api_client as client:
             return metrics_read.sync_detailed(
-                client=client, evaluation_id=evaluation_id
+                client=client, evaluation_experiment_id=evaluation_experiment_id
             )
 
 
 class EvaluationsApi(BaseApi):
     def __init__(self, client: AtriaHubClient):
         self._client = client
-        self._sample_results = EvaluationSampleResultApi(client)
+        self._sample_evaluations = SampleEvaluationApi(client)
         self._metrics = EvaluationMetricsApi(client)
+        self._sample_explanations = SampleExplanationsApi(client)
+        self._sample_explanation_metrics = SampleExplanationMetricsApi(client)
 
     @property
-    def sample_results(self) -> EvaluationSampleResultApi:
+    def sample_evaluations(self) -> SampleEvaluationApi:
         """Access to sample results API."""
-        return self._sample_results
+        return self._sample_evaluations
 
     @property
     def metrics(self) -> EvaluationMetricsApi:
         """Access to metrics API."""
         return self._metrics
+
+    @property
+    def sample_explanations(self) -> SampleExplanationsApi:
+        """Access to sample explanations API."""
+        return self._sample_explanations
+
+    @property
+    def sample_explanation_metrics(self) -> SampleExplanationMetricsApi:
+        """Access to sample explanation metrics API."""
+        return self._sample_explanation_metrics
 
     @api_error_handler
     def get_or_create(
@@ -120,18 +224,20 @@ class EvaluationsApi(BaseApi):
         model_id: UUID,
         model_branch: str,
         model_config_name: str,
-    ) -> ModelEvaluation:
+    ) -> EvaluationExperiment:
         """Retrieve a dataset from the hub by its name."""
 
-        from atriax_client.api.evaluations import evaluations_get_or_create
-        from atriax_client.models.model_evaluation_get_or_create import (
-            ModelEvaluationGetOrCreate,
+        from atriax_client.api.evaluation_experiments import (
+            evaluation_experiments_get_or_create,
+        )
+        from atriax_client.models.evaluation_experiment_get_or_create import (
+            EvaluationExperimentGetOrCreate,
         )
 
         with self._client.protected_api_client as client:
-            return evaluations_get_or_create.sync_detailed(
+            return evaluation_experiments_get_or_create.sync_detailed(
                 client=client,
-                body=ModelEvaluationGetOrCreate(
+                body=EvaluationExperimentGetOrCreate(
                     dataset_id=dataset_id,
                     dataset_branch=dataset_branch,
                     dataset_config_name=dataset_config_name,
